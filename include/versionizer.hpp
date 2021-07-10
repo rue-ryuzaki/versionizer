2ec9e83e7bc79aa0a8bdbd6ba965f441e07bc352
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2021-07-10 12:57:33 +0300

    Versionizer refactored

--- include/versionizer.hpp
+++ include/versionizer.hpp
@@ -36,28 +36,137 @@
 #define VERSIONIZER_VERSION_RC 0
 
 namespace versionizer {
+enum Type {
+    None,
+
+    M,
+    MM,
+    MR,
+    MMP,
+    MMR,
+    MMPR
+};
+
 struct Version
 {
     int major;
     int minor;
     int patch;
     int rc;
+
+    std::string to_string(Type type) const
+    {
+        validate(type);
+        switch (type) {
+            case M :
+                return std::to_string(major);
+            case MM :
+                return std::to_string(major)
+                        + "." + std::to_string(minor);
+            case MR :
+                if (rc == 0) {
+                    return std::to_string(major);
+                } else {
+                    return std::to_string(major)
+                            + "-rc" + std::to_string(rc);
+                }
+            case MMP :
+                return std::to_string(major)
+                        + "." + std::to_string(minor)
+                        + "." + std::to_string(patch);
+            case MMR :
+                if (rc == 0) {
+                    return std::to_string(major)
+                            + "." + std::to_string(minor);
+                } else {
+                    return std::to_string(major)
+                            + "-rc" + std::to_string(rc);
+                };
+            case MMPR :
+                if (rc == 0) {
+                    return std::to_string(major)
+                            + "." + std::to_string(minor)
+                            + "." + std::to_string(patch);
+                } else {
+                    return std::to_string(major)
+                            + "." + std::to_string(minor)
+                            + "-rc" + std::to_string(rc);
+                }
+            default :
+                throw std::logic_error("error: invalid version type");
+        }
+    }
+
+private:
+    void validate(Type type) const
+    {
+        switch (type) {
+            case M :
+                if (major < 0) {
+                    throw std::invalid_argument("error: invalid major version");
+                }
+                break;
+            case MM :
+                if (major < 0) {
+                    throw std::invalid_argument("error: invalid major version");
+                }
+                if (minor < 0) {
+                    throw std::invalid_argument("error: invalid minor version");
+                }
+                break;
+            case MR :
+                if (major < 0) {
+                    throw std::invalid_argument("error: invalid major version");
+                }
+                if (rc < 0) {
+                    throw std::invalid_argument("error: invalid rc version");
+                }
+                break;
+            case MMP :
+                if (major < 0) {
+                    throw std::invalid_argument("error: invalid major version");
+                }
+                if (minor < 0) {
+                    throw std::invalid_argument("error: invalid minor version");
+                }
+                if (patch < 0) {
+                    throw std::invalid_argument("error: invalid patch version");
+                }
+                break;
+            case MMR :
+                if (major < 0) {
+                    throw std::invalid_argument("error: invalid major version");
+                }
+                if (minor < 0) {
+                    throw std::invalid_argument("error: invalid minor version");
+                }
+                if (rc < 0) {
+                    throw std::invalid_argument("error: invalid rc version");
+                }
+                break;
+            case MMPR :
+                if (major < 0) {
+                    throw std::invalid_argument("error: invalid major version");
+                }
+                if (minor < 0) {
+                    throw std::invalid_argument("error: invalid minor version");
+                }
+                if (patch < 0) {
+                    throw std::invalid_argument("error: invalid patch version");
+                }
+                if (rc < 0) {
+                    throw std::invalid_argument("error: invalid rc version");
+                }
+                break;
+            default :
+                throw std::logic_error("error: invalid version type");
+        }
+    }
 };
 
 class Versionizer
 {
 public:
-    enum Type {
-        None,
-
-        M,
-        MM,
-        MR,
-        MMP,
-        MMR,
-        MMPR
-    };
-
     Versionizer()
         : m_type(None)
     { }
@@ -88,113 +197,10 @@ public:
 
     std::string versionToString(Version const& ver) const
     {
-        validateVersion(ver);
-        switch (m_type) {
-            case M :
-                return std::to_string(ver.major);
-            case MM :
-                return std::to_string(ver.major)
-                        + "." + std::to_string(ver.minor);
-            case MR :
-                if (ver.rc == 0) {
-                    return std::to_string(ver.major);
-                } else {
-                    return std::to_string(ver.major)
-                            + "-rc" + std::to_string(ver.rc);
-                }
-            case MMP :
-                return std::to_string(ver.major)
-                        + "." + std::to_string(ver.minor)
-                        + "." + std::to_string(ver.patch);
-            case MMR :
-                if (ver.rc == 0) {
-                    return std::to_string(ver.major)
-                            + "." + std::to_string(ver.minor);
-                } else {
-                    return std::to_string(ver.major)
-                            + "-rc" + std::to_string(ver.rc);
-                };
-            case MMPR :
-                if (ver.rc == 0) {
-                    return std::to_string(ver.major)
-                            + "." + std::to_string(ver.minor)
-                            + "." + std::to_string(ver.patch);
-                } else {
-                    return std::to_string(ver.major)
-                            + "." + std::to_string(ver.minor)
-                            + "-rc" + std::to_string(ver.rc);
-                }
-            default :
-                throw std::logic_error("error: invalid version type");
-        }
+        return ver.to_string(type());
     }
 
 private:
-    void validateVersion(Version const& ver) const
-    {
-        switch (m_type) {
-            case M :
-                if (ver.major < 0) {
-                    throw std::invalid_argument("error: invalid major version");
-                }
-                break;
-            case MM :
-                if (ver.major < 0) {
-                    throw std::invalid_argument("error: invalid major version");
-                }
-                if (ver.minor < 0) {
-                    throw std::invalid_argument("error: invalid minor version");
-                }
-                break;
-            case MR :
-                if (ver.major < 0) {
-                    throw std::invalid_argument("error: invalid major version");
-                }
-                if (ver.rc < 0) {
-                    throw std::invalid_argument("error: invalid rc version");
-                }
-                break;
-            case MMP :
-                if (ver.major < 0) {
-                    throw std::invalid_argument("error: invalid major version");
-                }
-                if (ver.minor < 0) {
-                    throw std::invalid_argument("error: invalid minor version");
-                }
-                if (ver.patch < 0) {
-                    throw std::invalid_argument("error: invalid patch version");
-                }
-                break;
-            case MMR :
-                if (ver.major < 0) {
-                    throw std::invalid_argument("error: invalid major version");
-                }
-                if (ver.minor < 0) {
-                    throw std::invalid_argument("error: invalid minor version");
-                }
-                if (ver.rc < 0) {
-                    throw std::invalid_argument("error: invalid rc version");
-                }
-                break;
-            case MMPR :
-                if (ver.major < 0) {
-                    throw std::invalid_argument("error: invalid major version");
-                }
-                if (ver.minor < 0) {
-                    throw std::invalid_argument("error: invalid minor version");
-                }
-                if (ver.patch < 0) {
-                    throw std::invalid_argument("error: invalid patch version");
-                }
-                if (ver.rc < 0) {
-                    throw std::invalid_argument("error: invalid rc version");
-                }
-                break;
-            default :
-                throw std::logic_error("error: invalid version type");
-        }
-    }
-
     Type m_type;
 };
 } // versionizer
