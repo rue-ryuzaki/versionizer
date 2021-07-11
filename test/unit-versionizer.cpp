0f71ffef62c9834bd2ddb280170f5f0500505177
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2021-07-11 21:41:37 +0300

    Added 'version change check' test cases

--- test/unit-versionizer.cpp
+++ test/unit-versionizer.cpp
@@ -68,3 +68,95 @@ TEST_CASE("versionToString() check", "[versionizer]")
         REQUIRE_THROWS(vizer.versionToString({-1,0,0,0}));
     }
 }
+
+TEST_CASE("version change check", "[versionizer]")
+{
+    auto vizer = versionizer::Versionizer();
+
+    SECTION("type M") {
+        vizer.setType("M");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_major(vizer.type())) == "1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_major(vizer.type())) == "2");
+    }
+
+    SECTION("type MM") {
+        vizer.setType("MM");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_major(vizer.type())) == "1.0");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_major(vizer.type())) == "2.0");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_minor(vizer.type())) == "0.1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_minor(vizer.type())) == "1.3");
+    }
+
+    SECTION("type MR") {
+        vizer.setType("MR");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_major(vizer.type())) == "1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_major(vizer.type())) == "1");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_rc(vizer.type())) == "1-rc1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_rc(vizer.type())) == "1-rc5");
+    }
+
+    SECTION("type MMP") {
+        vizer.setType("MMP");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_major(vizer.type())) == "1.0.0");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_major(vizer.type())) == "2.0.0");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_minor(vizer.type())) == "0.1.0");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_minor(vizer.type())) == "1.3.0");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_patch(vizer.type())) == "0.0.1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_patch(vizer.type())) == "1.2.4");
+    }
+
+    SECTION("type MMR") {
+        vizer.setType("MMR");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_major(vizer.type())) == "1.0");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,0}.apply_major(vizer.type())) == "2.0");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_major(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_minor(vizer.type())) == "0.1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,4}.apply_minor(vizer.type())) == "1.2");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_rc(vizer.type())) == "1-rc1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,0,3,4}.apply_rc(vizer.type())) == "1-rc5");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_rc(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_rc_major(vizer.type())) == "1-rc1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,0,3,4}.apply_rc_major(vizer.type())) == "1-rc5");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_rc_major(vizer.type()));
+    }
+
+    SECTION("type MMPR") {
+        vizer.setType("MMPR");
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_major(vizer.type())) == "1.0.0");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,0}.apply_major(vizer.type())) == "2.0.0");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_major(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_minor(vizer.type())) == "0.1.0");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,0,4}.apply_minor(vizer.type())) == "1.2.0");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_minor(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_patch(vizer.type())) == "0.0.1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,3,0}.apply_patch(vizer.type())) == "1.2.4");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_patch(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_rc(vizer.type())) == "0.1-rc1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,0,4}.apply_rc(vizer.type())) == "1.2-rc5");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_rc(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_rc_major(vizer.type())) == "1.0-rc1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,0,0,4}.apply_rc_major(vizer.type())) == "1.0-rc5");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_rc_major(vizer.type()));
+
+        REQUIRE(vizer.versionToString(versionizer::Version{0,0,0,0}.apply_rc_minor(vizer.type())) == "0.1-rc1");
+        REQUIRE(vizer.versionToString(versionizer::Version{1,2,0,4}.apply_rc_minor(vizer.type())) == "1.2-rc5");
+        REQUIRE_THROWS(versionizer::Version{1,2,3,4}.apply_rc_minor(vizer.type()));
+    }
+}
